1b03b4298075be6a9722b08ec16648639ce9ed02
Fernando Mitsugi Takagi <fernandotakagi23@gmail.com>
2022-09-04 13:28:57 -0300

    Redirecionamento do educador, ele não tem acesso

---
+++ monitor/beneficiarios_historicos.php
@@ -0,0 +1,78 @@
+<?php
+
+session_start();
+$skill = isset($_SESSION['skill']) ? $_SESSION['skill'] : null;
+$pdv = isset($_SESSION['pdv']) ? $_SESSION['pdv'] : null;
+$eps = isset($_SESSION['eps']) ? $_SESSION['eps'] : null;
+$nome = isset($_SESSION['nome']) ? $_SESSION['nome'] : null;
+$admin = isset($_SESSION['admin']) ? $_SESSION['admin'] : null;
+$id = isset($_SESSION['id']) ? $_SESSION['id'] : null;
+$url = isset($_SESSION['url']) ? $_SESSION['url'] : null;
+
+if (!$nome) {
+    echo ("<script LANGUAGE='JavaScript'>    
+    window.location.href='login.html';
+    </script>");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>FPA - Fundação Porta Aberta</title>
+
+    <link rel="icon" type="image/x-icon" href="./img/logo.ico">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <!-- <link rel="stylesheet" href="dist/css/adminlte.min.css"> -->
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="js/config.js?versao=1" type="text/javascript"></script>
+    <!-- Custom styles for this template-->
+    <link href="./css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+<!-- Carrega o Menu para a EPS - Administrador -->
+<?php if ($_SESSION['eps'] === '27') include __DIR__ . '/header/administrador_header.php'; ?>
+
+<!-- Carrega o Menu para a EPS - Diretor -->
+<?php if ($_SESSION['eps'] === '26') include __DIR__ . '/header/diretor_header.php'; ?>
+
+<!-- Controle de acessos para a EPS - Coordenador -->
+<?php if ($_SESSION['eps'] === '28') header('Location: beneficiarios.php'); ?>
+
+<!-- Controle de acessos para a EPS - Educador -->
+<?php if ($_SESSION['eps'] === '29') header('Location: beneficiarios.php'); ?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
